f065fccba91b3299516d318e1e09f7c588a97eae
cjrot <cjrot@DESKTOP-MDM1831>
2025-02-09 16:25:56 -0600

    Files

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace proj0App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace proj0App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
